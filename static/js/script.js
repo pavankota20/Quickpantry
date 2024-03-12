e93cf5fc7076b26875d2deb1125af7d3966cbb90
Pavan Kumar Kotapally <pavankumarkotapally@pavans-air.mynetworksettings.com>
2024-03-12 11:40:49 -0400

    completed

--- static/js/script.js
+++ static/js/script.js
@@ -116,64 +116,52 @@ function displayResults(data, resultsSectionId) {
     resultsSection.innerHTML = ''; // Clear previous results
     
     data.forEach(item => {     
-        const imageFormats = ['.png', '.jpg'];
-        fetch(`/find_image_formats?term=${encodeURIComponent(item['product_id'])}`)
-        .then(response => response.json())
-        .then(data => {
-            if (data == 'JPEG') {
-                data = 'jpg'
-            }
-            const card = document.createElement('div');
-            card.className =  'flex-none w-48 bg-white rounded-lg shadow-md p-4';
-            card.style.height = '250px';
-            
-            const image = document.createElement('img');
-            image.className = 'mx-auto mb-4';
-            image.src = 'static/Images/' + item['product_id'].toString() + '/000001.' + data.toLowerCase();
-            image.style.height = '100px';
-            
-            const cardContent = document.createElement('div');
-            cardContent.className = 'text-center text-sm';
-            cardContent.textContent = item['product_name']; 
-            cardContent.style.height = '50px';
-            
-            const addButton = document.createElement('button');
-            addButton.className = 'w-full bg-green-500 text-white rounded-full mt-4 py-2 flex items-center justify-center focus:outline-none';
-            addButton.style.width = '25%';
-            addButton.style.float = 'right';
-            
-            const addIcon = document.createElement('i');
-            addIcon.className = 'fas fa-plus mr-2';
-            addIcon.style.marginRight = '0px';
-            addButton.appendChild(addIcon);
-            
-            const viewButton = document.createElement('button');
-            viewButton.className = 'w-full bg-green-500 text-white rounded-full mt-4 py-2 flex items-center justify-center focus:outline-none';
-            viewButton.style.width = '25%';
-            viewButton.style.float = 'right';
-            viewButton.style.marginRight = '7%'
-            
-            const viewIcon = document.createElement('i');
-            viewIcon.className = 'fas fa-eye mr-2';
-            viewIcon.style.marginRight = '0px';
-            viewButton.appendChild(viewIcon);
-            
-            viewButton.addEventListener('click', function() {
-                selectedProduct = item;
-                selectedProductPath = 'static/Images/' + item['product_id'].toString() + '/000001.' + data.toLowerCase();
-                displayRecommendedProducts();
-            });
-            
-            card.appendChild(image);
-            card.appendChild(cardContent);
-            card.appendChild(addButton);
-            card.appendChild(viewButton);
-            resultsSection.appendChild(card);
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
+        const card = document.createElement('div');
+        card.className =  'flex-none w-48 bg-white rounded-lg shadow-md p-4';
+        card.style.height = '250px';
+        
+        const image = document.createElement('img');
+        image.className = 'mx-auto mb-4';
+        image.src = 'static/Images/' + item['product_id'].toString() + '/000001.jpg';
+        image.style.height = '100px';
+        
+        const cardContent = document.createElement('div');
+        cardContent.className = 'text-center text-sm';
+        cardContent.textContent = item['product_name']; 
+        cardContent.style.height = '50px';
+        
+        const addButton = document.createElement('button');
+        addButton.className = 'w-full bg-green-500 text-white rounded-full mt-4 py-2 flex items-center justify-center focus:outline-none';
+        addButton.style.width = '25%';
+        addButton.style.float = 'right';
+        
+        const addIcon = document.createElement('i');
+        addIcon.className = 'fas fa-plus mr-2';
+        addIcon.style.marginRight = '0px';
+        addButton.appendChild(addIcon);
+        
+        const viewButton = document.createElement('button');
+        viewButton.className = 'w-full bg-green-500 text-white rounded-full mt-4 py-2 flex items-center justify-center focus:outline-none';
+        viewButton.style.width = '25%';
+        viewButton.style.float = 'right';
+        viewButton.style.marginRight = '7%'
+        
+        const viewIcon = document.createElement('i');
+        viewIcon.className = 'fas fa-eye mr-2';
+        viewIcon.style.marginRight = '0px';
+        viewButton.appendChild(viewIcon);
+        
+        viewButton.addEventListener('click', function() {
+            selectedProduct = item;
+            selectedProductPath = 'static/Images/' + item['product_id'].toString() + '/000001.jpg' ;
+            displayRecommendedProducts();
         });
         
+        card.appendChild(image);
+        card.appendChild(cardContent);
+        card.appendChild(addButton);
+        card.appendChild(viewButton);
+        resultsSection.appendChild(card);   
     });
 }
 
@@ -182,59 +170,20 @@ function toggleModal() {
     modalOverlay.classList.toggle('hidden');
 }
 
-function displayRecommendedProducts() {
-    const resultsContainer = document.createElement('recommendations-container');
-    
-    const body = document.createElement('div');
-    body.className = 'results-container-0';
-    
-    const heading = document.createElement('div');
-    heading.className = 'flex items-center justify-between';
-    
-    const heading_1 = document.createElement('div');
-    heading_1.className = 'text-xl font-semibold';
-    heading_1.textContent = 'Recommendations';
-    
-    const content = document.createElement('div');
-    content.className = 'flex items-center';
-    
-    const scrollLeftBtn = document.createElement('button');
-    scrollLeftBtn.className = 'text-2xl text-gray-600 mr-4';
-    
-    const leftIcon = document.createElement('i');
-    scrollLeftBtn.id = 'scroll-left-0' ;
-    leftIcon.className = 'fas fa-chevron-left';
-    
-    const scrollRightBtn = document.createElement('button');
-    scrollRightBtn.id = 'scroll-right-0';
-    scrollRightBtn.className = 'text-2xl text-gray-600';
-    
-    const rightIcon = document.createElement('i');
-    rightIcon.className = 'fas fa-chevron-right';
-    
-    scrollLeftBtn.appendChild(leftIcon);
-    scrollRightBtn.appendChild(rightIcon);
-    content.appendChild(scrollLeftBtn);
-    content.appendChild(scrollRightBtn);
-    heading.append(heading_1);
-    heading.append(content);
-    body.appendChild(heading);
-    
-    const resultsSection = document.createElement('div');
-    resultsSection.id = 'results-section-0';
-    resultsSection.className = 'flex overflow-x-auto py-6 space-x-4';
-    body.appendChild(resultsSection);
-    resultsContainer.append(body);
-    
-    document.getElementById(scrollLeftBtn.id).addEventListener('click', () => {
-        document.getElementById(resultsSection.id).scrollBy({ left: -300, behavior: 'smooth' });
-    });
+function openModal() {
+    const modalOverlay = document.getElementById('modalOverlay');
+    modalOverlay.classList = 'overlay';
+}
 
-    document.getElementById(scrollRightBtn.id ).addEventListener('click', () => {
-        document.getElementById(resultsSection.id).scrollBy({ left: 300, behavior: 'smooth' });
-    });
-    
-    
+function closeModal() {
+    const modalOverlay = document.getElementById('modalOverlay');
+    modalOverlay.classList = 'hidden';
+    selectedProduct = null;
+    selectedProductPath = '';
+    modalOverlay.remove();
+}
+
+function displayRecommendedProducts() {
     fetch(`/get_product_recommendations`, {
         method: 'POST',
         headers: {
@@ -244,21 +193,65 @@ function displayRecommendedProducts() {
     })
     .then(response => response.json())
     .then(data => {
-        data.forEach(item => {     
-            const imageFormats = ['.png', '.jpg'];
-            fetch(`/find_image_formats?term=${encodeURIComponent(item['product_id'])}`)
-            .then(response => response.json())
-            .then(data => {
-                if (data == 'JPEG') {
-                    data = 'jpg'
-                }
+            const resultsContainer = document.createElement('recommendations-container');
+            
+            const body = document.createElement('div');
+            body.className = 'results-container-0';
+            
+            const heading = document.createElement('div');
+            heading.className = 'flex items-center justify-between';
+            
+            const heading_1 = document.createElement('div');
+            heading_1.className = 'text-xl font-semibold';
+            heading_1.textContent = 'Recommendations';
+            
+            const content = document.createElement('div');
+            content.className = 'flex items-center';
+            
+            const scrollLeftBtn = document.createElement('button');
+            scrollLeftBtn.className = 'text-2xl text-gray-600 mr-4';
+            
+            const leftIcon = document.createElement('i');
+            scrollLeftBtn.id = 'scroll-left-0' ;
+            leftIcon.className = 'fas fa-chevron-left';
+            
+            const scrollRightBtn = document.createElement('button');
+            scrollRightBtn.id = 'scroll-right-0';
+            scrollRightBtn.className = 'text-2xl text-gray-600';
+            
+            const rightIcon = document.createElement('i');
+            rightIcon.className = 'fas fa-chevron-right';
+            
+            scrollLeftBtn.appendChild(leftIcon);
+            scrollRightBtn.appendChild(rightIcon);
+            content.appendChild(scrollLeftBtn);
+            content.appendChild(scrollRightBtn);
+            heading.append(heading_1);
+            heading.append(content);
+            body.appendChild(heading);
+            
+            const resultsSection = document.createElement('div');
+            resultsSection.id = 'results-section-0';
+            resultsSection.className = 'flex overflow-x-auto py-6 space-x-4';
+            body.appendChild(resultsSection);
+            resultsContainer.append(body);
+            
+            document.getElementById(scrollLeftBtn.id).addEventListener('click', () => {
+                document.getElementById(resultsSection.id).scrollBy({ left: -300, behavior: 'smooth' });
+            });
+
+            document.getElementById(scrollRightBtn.id ).addEventListener('click', () => {
+                document.getElementById(resultsSection.id).scrollBy({ left: 300, behavior: 'smooth' });
+            });
+            
+            data.forEach(item => {
                 const card = document.createElement('div');
                 card.className =  'flex-none w-48 bg-white rounded-lg shadow-md p-4';
                 card.style.height = '250px';
                 
                 const image = document.createElement('img');
                 image.className = 'mx-auto mb-4';
-                image.src = 'static/Images/' + item['product_id'].toString() + '/000001.' + data.toLowerCase();
+                image.src = 'static/Images/' + item['product_id'].toString() + '/000001.jpg';
                 image.style.height = '100px';
                 
                 const cardContent = document.createElement('div');
@@ -279,55 +272,47 @@ function displayRecommendedProducts() {
                 card.appendChild(image);
                 card.appendChild(cardContent);
                 card.appendChild(addButton);
-                resultsSection.appendChild(card);
-            })
-            .catch(error => {
-                console.error('Error fetching data:', error);
-            });          
-        });       
-    })
-    .then(data => {
-        const modalMain = document.createElement('div');
-        
-        const modalHeading = document.createElement('h2');
-        modalHeading.id = selectedProduct['product_id'];
-        modalHeading.className = 'text-lg font-semibold mb-4';
-        modalHeading.textContent = selectedProduct['product_name'];
-        
-        const modalImage = document.createElement('img');
-        modalImage.className = 'mx-auto mb-4';
-        modalImage.src = selectedProductPath;
-        
-        modalMain.appendChild(modalHeading);
-        modalMain.appendChild(modalImage);
-        modalMain.appendChild(resultsContainer);
-        
-        const createModalbody = document.getElementById('body');
-        
-        const createModalmain = document.createElement('div');
-        createModalmain.className = 'overlay hidden';
-        createModalmain.id = 'modalOverlay';
-        
-        const createModalmodal = document.createElement('div');
-        createModalmodal.className = 'modal';
-        
-        const createModalcloseButton = document.createElement('button');
-        createModalcloseButton.className = 'bg-white border border-gray-300 text-gray-700 py-2 px-4 rounded';
-        
-        createModalcloseButton.addEventListener('click', function() {
-            selectedProduct = null;
-            selectedProductPath = '';
-            modalHeading.remove();
-            modalImage.remove();
-            resultsSection.remove();
-            toggleModal();
-        });
-        
-        createModalmodal.appendChild(modalMain); 
-        createModalmodal.appendChild(createModalcloseButton);
-        createModalmain.appendChild(createModalmodal);
-        createModalbody.appendChild(createModalmain);
-        toggleModal();  
+                resultsSection.appendChild(card);   
+            }); 
+            
+            const modalMain = document.createElement('div');
+            
+            const modalHeading = document.createElement('h2');
+            modalHeading.id = selectedProduct['product_id'];
+            modalHeading.className = 'text-lg font-semibold mb-4';
+            modalHeading.textContent = selectedProduct['product_name'];
+            
+            const modalImage = document.createElement('img');
+            modalImage.className = 'mx-auto mb-4';
+            modalImage.src = selectedProductPath;
+            modalImage.style.height = '200px';
+            
+            modalMain.appendChild(modalHeading);
+            modalMain.appendChild(modalImage);
+            modalMain.appendChild(resultsContainer);
+            
+            const createModalbody = document.getElementById('body');
+            
+            const createModalmain = document.createElement('div');
+            createModalmain.className = 'overlay hidden';
+            createModalmain.id = 'modalOverlay';
+            
+            const createModalmodal = document.createElement('div');
+            createModalmodal.className = 'modal';
+            
+            const createModalcloseButton = document.createElement('button');
+            createModalcloseButton.className = 'bg-white border border-gray-300 text-gray-700 py-2 px-4 rounded';
+            createModalcloseButton.textContent = 'Close';
+            
+            createModalcloseButton.addEventListener('click', function() {
+                closeModal();
+            });
+            
+            createModalmodal.appendChild(modalMain); 
+            createModalmodal.appendChild(createModalcloseButton);
+            createModalmain.appendChild(createModalmodal);
+            createModalbody.appendChild(createModalmain); 
+            openModal();                 
     })
     .catch(error => {
         console.error(error)
